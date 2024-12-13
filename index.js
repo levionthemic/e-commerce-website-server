655cd7a17777507658da2a2eeefcd1a9f0d77a3f
Ho Tran Ngoc Liem <ngocliem10a5nth@gmail.com>
2024-12-13 10:02:44 +0700

    Fix CORS

--- index.js
+++ index.js
@@ -24,6 +24,7 @@ app.use(cors());
 database.connect();
 
 // Routes
+app.options('*', cors());
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
