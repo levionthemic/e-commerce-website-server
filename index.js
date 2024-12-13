0ab579920fb795e5dc8b85f48c64839612f90586
Ho Tran Ngoc Liem <ngocliem10a5nth@gmail.com>
2024-12-13 10:59:15 +0700

    fix

--- index.js
+++ index.js
@@ -21,18 +21,7 @@ app.use(cookieParser());
 // Database
 database.connect();
 
-app.use(function (req, res, next) {
-  // Allow access request from any computers
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
-  res.header('Access-Control-Allow-Methods', 'POST, GET, PUT, DELETE,PATCH');
-  res.header('Access-Control-Allow-Credentials', true);
-  if ('OPTIONS' == req.method) {
-    res.sendStatus(200);
-  } else {
-    next();
-  }
-});
+app.use(cors());
 
 // Routes
 adminRoutes(app);
