7167212953696fbb30d980330ec4b3ac6fd5208a
Ho Tran Ngoc Liem <ngocliem10a5nth@gmail.com>
2024-12-13 10:19:12 +0700

    Fix bug

--- index.js
+++ index.js
@@ -18,18 +18,17 @@ const port = process.env.PORT;
 app.use(bodyParser.json());
 app.use(cookieParser());
 
-app.use(cors());
+const corsOptions ={
+  origin:'*', 
+  credentials:true,            //access-control-allow-credentials:true
+  optionSuccessStatus:200,
+}
+app.use(cors(corsOptions));
 
 // Database
 database.connect();
 
 // Routes
-app.options('*', cors());
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-});
 adminRoutes(app);
 clientRoutes(app);
 sellerRoutes(app);
